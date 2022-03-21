c7856524d875635f52a4a0a84d3e6f56acda3edc
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-21 07:01:27 +0000

    added WASD movements

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 // setup interface to handle user input from stdin
+let connection;
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,8 +14,17 @@ const setupInput = function () {
 const handleUserInput = key => {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up")
+  } else if (key === 'a') {
+    connection.write("Move: left")
+  } else if (key === 's') {
+    connection.write("Move: down")
+  } else if (key === 'd') {
+    connection.write("Move: right")
   }
-};
+    
+  };
 
 module.exports = { setupInput };
 
